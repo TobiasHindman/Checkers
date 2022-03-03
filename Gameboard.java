3e3f120a85af3fe55b60ff6c0c912c8437a9b353
Tobias Hindman <100604694+TobiasHindman@users.noreply.github.com>
2022-03-03 16:20:29 +0000

    Added things

--- Gameboard.java
+++ Gameboard.java
@@ -1,1 +1,12 @@
+import java.awt.*;
+import java.swing.*;
+public class GameBoard {
+  JFrame frame = new JFrame("My Drawing");
+  Jpanel panel1 = new Jpanel();
+  frame.setSize(400, 400);
+  panel1.setSize(300,300);
+  frame.setVisible(true);
+  panel1.setVisible(true);
+    
+}
 
