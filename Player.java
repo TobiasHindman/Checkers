c2a1075258ae940adbf1e1dd6fcf330366b22ef6
Abhinav Manosh Manosh <100604967+Abhinav18080@users.noreply.github.com>
2022-03-04 15:18:49 +0000

    add

--- Player.java
+++ Player.java
@@ -1,4 +1,24 @@
+import java.util.*;
+
 public class Player
 {
+  private String player;
+  private int numCheckers;
+
+  public Player(String p1)
+  {
+    player = p1;
+    numCheckers = 12;
+  }
+
+  public String getPlayer()
+  {
+    return player;
+  }
+
+  public int getCheckers();
+  {
+    return numCheckers; 
+  }
   
 }
