99d823f0c7345348c0241462cde5cbc55b5b8c0e
Abhinav Manosh Manosh <100604967+Abhinav18080@users.noreply.github.com>
2022-03-08 16:33:26 +0000

    stuff from today

--- Gameboard.java
+++ Gameboard.java
@@ -4,9 +4,24 @@ public class GameBoard {
   JFrame frame = new JFrame("My Drawing");
   Jpanel panel1 = new Jpanel();
   frame.setSize(400, 400);
-  panel1.setSize(300,300);
+  //panel1.setSize(300,300);
   frame.setVisible(true);
-  panel1.setVisible(true);
+  //panel1.setVisible(true);
+  public void generateBoard(Graphics g){
+    g.setColor(Color.BLACK);
+    g.drawRect(200,200,300,300);
+    g.setColor(Color.WHITE);
+    g.drawRect(65,65,30,30);
+    g.drawRect(130,65,30,30);
+    g.drawRect(195,65,30,30);
+    g.drawRect(260,65,30,30);
+    g.drawRect(325,65,30,30);
+  }  
+  public Array gameBoard = new Array[][];
+  public void moveRedPiece(int x, int y){
     
+  }
+  public void moveBluePiece(int x,int y){
+    
+  }
 }
-
--- Player.java
+++ Player.java
@@ -4,6 +4,9 @@ public class Player
 {
   private String player;
   private int numCheckers;
+  private boolean run; 
+  private Random rand = new Random();
+  private int num; 
 
   public Player(String p1)
   {
@@ -16,9 +19,43 @@ public class Player
     return player;
   }
 
-  public int getCheckers();
+  public int getCheckers()
   {
     return numCheckers; 
   }
-  
+
+  public void ai()
+  {
+    numCheckers = 12;
+    player = "AI";
+
+    if (numCheckers == 0)
+    {
+      checkWin();
+    }
+
+    if(numCheckers < 12)
+    {
+      
+    }
+    else
+    {
+      num = rand.nextInt(); 
+    }
+    
+  }
+
+  public void checkWin()
+  {
+    if (numCheckers == 0)
+    {
+      run = false; 
+      System.out.println(player +" lost");
+    }
+    else
+    {
+      run = true;
+      System.out.println(player + " won");
+    }
+  }
 }
