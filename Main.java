10cc4b9b943a96bfd085390ef331862b99ceb543
Abhinav Manosh Manosh <100604967+Abhinav18080@users.noreply.github.com>
2022-03-11 15:31:49 +0000

    switchPlayer();

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import Player.java; 
+
+public class Main
+  {
+    public static void main(String[] args)
+    {
+      Player p1 = new Player();
+      p1.switchPlayer();
+    }
+  }
--- Player.java
+++ Player.java
@@ -4,20 +4,35 @@ public class Player
 {
   private String player;
   private int numCheckers;
+  private String player2; 
+  private String currentPlayer;
   private boolean run = true;
   private Scanner kb = new Scanner(System.in);
   private String row;  
-  private String col; 
+  private int col; 
+  private char cRow;
 
-  public Player(String p1, int nchecker)
+  public Player()
+  {
+    player = "Unkown";
+    player2 = "Unkown";
+    numCheckers = 12; 
+  }
+  public Player(String p1, String p2)
   {
     player = p1;
-    numCheckers = nchecker;
+    player2 = p2; 
+    numCheckers = 12;
   }
 
   public String getPlayer()
   {
     return player;
+  }
+
+  public String getPlayer2()
+  {
+    return player2; 
   }
 
   public int getCheckers()
@@ -34,15 +49,27 @@ public class Player
         System.out.print("Enter the x cordinate: ");
         row = kb.nextLine();
         row.toUpperCase();
-      }while(!row.equals("a"));
+        cRow = row.charAt(0);
+        if (cRow < 65 && cRow > 75)
+        {
+          System.out.println("Not possible");
+        }
+      }while(cRow < 65 && cRow > 75);
 
       do
       {
         System.out.print("Enter the y cordinate: ");
-        col = kb.nextLine();
-        col.toUpperCase();
-      }while(!col.equals("a"));
-      run = false; 
+        col = kb.nextInt(); 
+        if(col < 0 && col > 10)
+        {
+          System.out.println("Not possbile"); 
+        }
+      }while(col < 0 && col > 10);
+
+      if (numCheckers == 0)
+      {
+        break; 
+      }
     }
   }
 
