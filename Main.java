ba609e00c81ba3b7723c0e56ddcf54c5bc524fef
Tobias Hindman <100604694+TobiasHindman@users.noreply.github.com>
2022-03-11 15:30:25 +0000

    Added generate board methods

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main(){
+  Player player1 = new Player();
+  Player player2 = new Player();
+  boolean hasWon = false;
+  int count = 0;
+  while(hasWon){
+    if((count%2)==0){
+      
+    }
+    else{
+      
+    }
+    count++;
+  }
+}
