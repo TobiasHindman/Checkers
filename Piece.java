9532dfbad3ed35f964583a3f0915f550248f4444
Rithboss <rithvikvanga@gmail.com>
2022-03-21 10:26:37 -0500

    Changes for piece

--- Piece.java
+++ Piece.java
@@ -1,24 +1,39 @@
 public class Piece {
-    private  GameBoard currentGameBoard;
+    private  Gameboard currentGameBoard;
     private int xindex;
     private int yindex;
     public int color;
-    public piece(GameBoard currentBoard, int xindex1, int yindex1, String color1)  {
+    public boolean isKing;
+    public piece(GameBoard currentBoard, int xindex1, int yindex1, String color1, boolean isKing1)  {
         currentGameBoard = currentBoard;
         xindex = xindex1;
         yindex = yindex1;
         color = color1;
+        isKing = isKing1;
     }
     public void move(int toXIndex, int toYIndex) {
-        currentGameBoard.game[toXIndex -1][toYIndex -1] = currentGameBoard.game[xindex - 1][yindex - 1];
-        currentGameBoard.game[xindex - 1][yindex - 1] = null;
-        xindex = toXIndex;
-        yindex = toYIndex;
+        if(checkMove(toXIndex, toYIndex))  {
+            currentGameBoard.game[toXIndex -1][toYIndex -1] = currentGameBoard.game[xindex - 1][yindex - 1];
+            currentGameBoard.game[xindex - 1][yindex - 1] = null;
+            if (color = "red") {
+                currentGameBoard.moveRedPiece(xindex, yindex, toXIndex, toYIndex);
+            }
+            if (color = "blue") {
+                currentGameBoard.moveBluePiece(xindex, yindex, toXIndex, toYIndex);
+            }
+            xindex = toXIndex;
+            yindex = toYIndex;
+        }
+        else {}
+        
     }
     public void take(int takeXIndex, int takeYIndex, int toXIndex, int toYIndex) {
-        currentGameBoard.game[toXIndex -1][toYIndex -1] = currentGameBoard.game[xindex - 1][yindex - 1];
-        xindex = toXIndex;
-        yindex = toYIndex;
+        if (checkTake(takeXIndex, takeYIndex, toXIndex, toYIndex)) {
+            currentGameBoard.game[toXIndex -1][toYIndex -1] = currentGameBoard.game[xindex - 1][yindex - 1];
+            xindex = toXIndex;
+            yindex = toYIndex;
+        }
+        else {} 
     }
     public boolean checkMove(int toXIndex, int toYIndex){
         if(currentGameBoard[toXIndex][toYIndex] = null){
@@ -28,7 +43,7 @@ public class Piece {
             return false;
         }
     }
-    public boolean checkTake(int takeXIndex, int takeYIndex, intc toXIndex, int toYIndex){
+    public boolean checkTake(int takeXIndex, int takeYIndex, int toXIndex, int toYIndex){
         if(color = "blue") {
             if (toYIndex > takeYIndex && toXIndex > takeXIndex) {
                 return true;
@@ -41,7 +56,16 @@ public class Piece {
             }
             else {return false;}
         }
-
     }
+    public boolean checkDoubleJump(int takeXIndex, int takeYIndex,int takeXIndex2, int takeYIndex2, int toXIndex , int toYIndex) {
+        if (checkTake(takeXIndex, takeYIndex, takeXIndex2, takeYIndex2)) {
+            if(checkTake(takeXIndex2, takeYIndex2, toXIndex, toYIndex)) {
+                return true;
+            }
+            else{return false;}
+        }
+        else{return false;}  
+    }
+
 }
 
