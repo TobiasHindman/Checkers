ca490f51441a81e148f6b8d66c83f97d33118d20
Rithboss <rithvikvanga@gmail.com>
2022-03-24 10:16:21 -0500

    Updated computer class

--- Computer.java
+++ Computer.java
@@ -2,10 +2,28 @@ import math.random;
 public class Computer {
     public int nextLetter;
     public int nextNumber;
+    public Piece piece;
 
     public int[] nextMove() {
         nextLetter = (int) 10 * math.random(0,1);
         nextNumber = (int) 10 * math.random(0,1);
-        int[] nextMove = new int [nextLetter, nextNumber];
+        int[] nextMove = new int [2];
+        nextMove[0] = nextLetter;
+        nextMove[1] = nextNumber;
+        return nextMove;
+    }
+    public int[] fromMove() {
+        
+        while() {
+            nextLetter = (int) 10 * math.random(0,1);
+            nextNumber = (int) 10 * math.random(0,1);
+            int[] nextMove = new int [2];
+            nextMove[0] = nextLetter;
+            nextMove[1] = nextNumber;
+            if (piece.currentGameBoard[nextLetter - 1][ nextNumber - 1] != 0) {
+                return nextMove;
+            }
+        }
+        return null;
     }
 }
