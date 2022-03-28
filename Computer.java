a722e0a829d5f20271aa2bb4f294fc0bca31b043
Rithboss <rithvikvanga@gmail.com>
2022-03-28 10:34:26 -0500

    Monday

--- Computer.java
+++ Computer.java
@@ -13,17 +13,39 @@ public class Computer {
         return nextMove;
     }
     public int[] fromMove() {
-        
-        while() {
+        while(true){
             nextLetter = (int) 10 * math.random(0,1);
             nextNumber = (int) 10 * math.random(0,1);
             int[] nextMove = new int [2];
             nextMove[0] = nextLetter;
             nextMove[1] = nextNumber;
-            if (piece.currentGameBoard[nextLetter - 1][ nextNumber - 1] != 0) {
+            if (gameboard.GameBoard[nextLetter - 1][ nextNumber - 1] != 0) {
                 return nextMove;
             }
         }
         return null;
     }
-}
+    public int[] toMove(int[] fromMove, int color) {
+        int[] nextMove = new int [2];
+        if(color == 1) {
+            if(piece.checkMove(fromMove[0] + 1, fromMove[1] - 1)) {
+            nextMove[0] = fromMove[0] + 1;
+            nextMove[1] = fromMove[0] - 1;
+            }
+            if(piece.checkMove(fromMove[0] - 1, fromMove[1] - 1)){
+            nextMove[0] = fromMove[0] + 1;
+            nextMove[1] = fromMove[0] - 1;
+            }
+        }
+        if(color == -1) {
+            if(piece.checkMove(fromMove[0] + 1, fromMove[1] + 1)) {
+            nextMove[0] = fromMove[0] + 1;
+            nextMove[1] = fromMove[0] + 1;
+            }
+            if(piece.checkMove(fromMove[0] - 1, fromMove[1] + 1))
+            nextMove[0] = fromMove[0] + 1;
+            nextMove[1] = fromMove[0] + 1;
+            }
+        } 
+    }
+
